a1da56837e0a0103e636f0f7cb18c5798c35f067
Yusuke Kawashima <y-kawashima@hoshi.ac.jp>
2021-06-03 18:39:10 +0900

    fix

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
 class Page(Enum):
-    ALL = '/html/body/div[2]/div[1]/div/div[2]/span[1]'
-    REPR = '/html/body/div[2]/div[1]/div/div[2]/span[2]'
-    LATEST = '/html/body/div[2]/div[1]/div/div[2]/span[3]'
+    ALL = '/html/body/div[1]/div/div/div[2]/a[1]' # allのタブ
+    REPR = '/html/body/div[1]/div/div/div[2]/a[2]' # 代表構造のタブ
+    LATEST = '/html/body/div[1]/div/div/div[2]/a[3]' # 
 
 class PdbjScraper:
 
@@ -23,10 +23,11 @@ class PdbjScraper:
         actions.move_to_element(element)
         actions.click()
         actions.perform()
-        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[1]/div/div[3]/div/div')))
+        sleep(3)
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div/div[3]/div'))) # 下のメニューバー(ページ番号)
 
         # pdbid順
-        element = driver.find_element_by_xpath('//*[@id="pageDIV"]/div[1]/aside/div[1]/div[2]/label[1]')
+        element = driver.find_element_by_xpath('/html/body/div[1]/div/aside/div[2]/div[2]/label[1]') # 右のpdbid順の場所
         actions = ActionChains(driver)
         element.location_once_scrolled_into_view
         actions.move_to_element(element)
@@ -37,25 +38,32 @@ class PdbjScraper:
     def read_page(self, driver, target, xpaths):
         data = []
         self.move_page(driver, target)
-        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[1]/div/div[3]/div/div')))
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div/div[3]/div'))) # 下のメニューバー(ページ番号)
         while True:
             tree = html.fromstring((driver.page_source).encode('utf-8'))
-            for i in  tree.xpath("/html/body/div[2]/div[1]/div/div[3]/table/tbody/tr[*]"):
+            for i in  tree.xpath('/html/body/div[1]/div/div/div[3]/table/tbody/tr[*]'):
                 data.append(["" if len(i.xpath(v)) == 0 else "".join(i.xpath(v)[0].itertext()) for k, v in xpaths.items()])
             try:
-                driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div[3]/div/div/a[text() = ">"]').click()
-                WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[@class = "loadingImage" and @class != "visible"]')))
+                driver.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div/a[text() = ">"]').click()  # 下のメニューバーの>ボタン
+                sleep(3)
+                #WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '/html/body/img[ @class == ""]'))) # >を押したときに出る画像
             except:
                 break
         return data
 
 
 if __name__ == "__main__":
+    from argparse import ArgumentParser
+    p = ArgumentParser()
+    p.add_argument("-v", action="store_true")
+    a = p.parse_args()
     opts = webdriver.ChromeOptions()
-    opts.add_argument('--headless')
+    if not a.v:
+        opts.add_argument('--headless')
     with webdriver.Chrome(options=opts) as driver:
         url= "https://pdbj.org/featured/covid-19"
         driver.get(url)
+        driver.set_window_size(2000,2000)
         sleep(5)
         scraper = PdbjScraper()
         xpaths = {
